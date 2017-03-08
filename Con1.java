11a96f7711c12e6b2e53a1bbae77df3ca40f1f4f
Samad Bhuiyan <masb80@users.noreply.github.com>
2017-03-08 08:19:43 -0330

    Constructor

---
+++ Con1.java
@@ -0,0 +1,12 @@
+
+public class Con1 {
+	double a;
+	String b;
+	Con1(double num, String name){
+		a = num ;
+		b = name;		
+	}
+ public void display(){
+	 System.out.println("Car Name " + b +" , "+ "Price = " + a );
+ }
+}
---
+++ Constructor.java
@@ -0,0 +1,10 @@
+
+public class Constructor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Con1 c = new Con1(3200, "Toyota");
+		c.display();
+	}
+
+}
