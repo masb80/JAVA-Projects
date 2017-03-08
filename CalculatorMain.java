0f0dc8175ab16adf81c6f63e154cc0c979419514
Samad Bhuiyan <masb80@users.noreply.github.com>
2017-03-08 08:18:52 -0330

    Calculator practice

---
+++ CalculatorMain.java
@@ -0,0 +1,11 @@
+// THis program is written by me, Samad not using editor
+public class CalculatorMain extends Calculator_ui{
+
+	public static void main(String[] args) {
+		Calculator_ui n = new Calculator_ui();
+		n.ui();
+		
+
+	}
+
+}
